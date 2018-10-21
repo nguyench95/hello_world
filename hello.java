1ecdb38a13c10f08f7506f5a7c69e62801b9dc97
nguyench95 <44301989+nguyench95@users.noreply.github.com>
2018-10-21 02:34:15 -0400

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,19 @@
+// a simple program to practice common Github commands 
+
+public class hello {
+
+    public static void main(String [] args) {
+
+
+        String firstName = "Christopher";
+        String lastName = "Nguyen";
+	
+        System.out.println("Hello, my name is " + firstName + " " + lastName);
+
+
+
+    }
+}
+
+
+`
